8b8166f075710836d958c29cd79a93d31bdf496c
Philip Granatuk <phil13@bk.ru>
2022-08-30 09:02:52 +0500

    15PW: third iteration (1)

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-err');
 
-const { NODE_ENV, SECRET_KEY } = process.env;
+const { NODE_ENV, JWT_SECRET} = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? SECRET_KEY : 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     throw new AuthError('Необходима авторизация');
   }
